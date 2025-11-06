3ab8bb6518e97782841840e743cfe4dc46b09e02
Avetisyan528 <slavik.avetisyan.o@gmail.com>
2025-11-06 22:36:56 +0400

    Add logo and update App layout with 'COMING SOON'
    Added personaLogo.png and updated App.tsx to display the logo at the top, a 'COMING SOON' heading, and improved contact information formatting.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import './App.css';
+import logo from '../static/personaLogo.png'; // replace with your image path
 
 function App() {
-  return (
-    <div className="App">
-        423814 РТ, г. Набережные Челны, Хлебный проезд 21.
-        Тел. Горячей линии в РФ:
-        8 8006005635
-        info@personatat.com
-        personatat.com
-        Логинов Сергей Александрович
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* Image at the top */}
+            <img src={logo} alt="Logo" style={{ maxWidth: '100%', height: 'auto' }} />
+
+            {/* COMING SOON text */}
+            <h1>COMING SOON</h1>
+
+            {/* Contact info */}
+            <div className="contact-info">
+                423814 РТ, г. Набережные Челны, Хлебный проезд 21.<br />
+                Тел. Горячей линии в РФ: 8 800 600 5635<br />
+                info@personatat.com<br />
+                personatat.com<br />
+                Логинов Сергей Александрович
+            </div>
+        </div>
+    );
 }
 
 export default App;
