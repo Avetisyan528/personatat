94c78d50815a6a4a1cd18073cebcb51194d30b86
Avetisyan528 <slavik.avetisyan.o@gmail.com>
2025-11-06 21:03:09 +0400

    Initialize React TypeScript project
    Add initial project files generated by Create React App with TypeScript template, including configuration, source files, public assets, and documentation.

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        423814 РТ, г. Набережные Челны, Хлебный проезд 21.
+        Тел. Горячей линии в РФ:
+        8 8006005635
+        info@personatat.com
+        personatat.com
+        Логинов Сергей Александрович
+    </div>
+  );
+}
+
+export default App;
